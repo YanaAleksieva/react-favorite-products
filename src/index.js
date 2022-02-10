a5a263102aefa8ead65d377d83d3c5fe536dbe1b
YanaAleksieva <ya.aleksieva@gmail.com>
2022-02-10 15:46:28 +0200

    remove unneeded redux content

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
-import productReducer from "./store/reducers/products";
 import ProductsProvider from "./context/products-context";
 
 ReactDOM.render(
